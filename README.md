234520da6741bb91d4da118f3c88efdb4c667e82
songsong <1040496928@qq.com>
2017-12-28 16:06:07 +0800

    升级
    升级库v1.2，添加压缩

--- README.md
+++ README.md
@@ -16,24 +16,28 @@ allprojects {
 
 ```groovy
 dependencies {
-	compile 'com.github.songsongtao:fast_code:v1.1'
+	compile 'com.github.songsongtao:fast_code:v1.2'
 }
 ```
 ## Features
 
+### v1.2
+
+- 添加压缩工具`Compressor`
+
 ### v1.1
 
 - 丰富工具类
-- 封装提示框，popWindow
-- 点赞view,空布局，正方形布局，banner,带删除的EditText等
+- 封装提示框，`popWindow`
+- 点赞view,空布局，正方形布局，`banner`,带删除的`EditText`等
 
 ### v1.0
 
 - 基本的工具类
-- android6.0权限封装
-- android7.0文件共享封装
-- 自定义TextView/RelativeLayout,支持边框,圆角,按压效果等
+- `android6.0`权限封装
+- `android7.0`文件共享封装
+- 自定义`TextView/RelativeLayout,`支持边框,圆角,按压效果等
 - 图片压缩封装,以及简单异步批量处理图片
 
 ## History
-- MPermissionUtils(内存泄漏)改为SPermissionUtils
+- `MPermissionUtils`(内存泄漏)改为`SPermissionUtils`
---
+++ fastlib/src/main/java/com/song/fast/compressor/ImageUtil.java
@@ -0,0 +1,126 @@
+package com.song.fast.compressor;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * description: compressImage
+ * author: ss
+ * date: 2017/12/28 10:08
+ * update: change compressImage calculateInSampleSize
+ * version: 1.0
+ */
+
+public class ImageUtil {
+
+    //compress main way
+    static File compressImage(File imageFile, long size, int reqWith, int reqHeight, Bitmap.CompressFormat compressFormat,
+                              int quantity, String destinationPath) throws IOException {
+
+        FileOutputStream fileOutputStream = null;
+        File file = new File(destinationPath).getParentFile();
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+
+        try {
+            fileOutputStream = new FileOutputStream(destinationPath);
+
+            Bitmap bitmap = decodeSampledBitmapFromFile(imageFile, reqWith, reqHeight);
+
+            //ByteArrayOutputStream don't need close
+            ByteArrayOutputStream baos = compressBitmapSize(bitmap, compressFormat, quantity, size);
+            fileOutputStream.write(baos.toByteArray());
+
+        } finally {
+            if (fileOutputStream != null) {
+                fileOutputStream.flush();
+                fileOutputStream.close();
+            }
+
+        }
+        return new File(destinationPath);
+    }
+
+
+    //compress and rotate bitmap
+    static Bitmap decodeSampledBitmapFromFile(File imageFile, int reqWidth, int reqHeight) throws IOException {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        //just need size
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(imageFile.getAbsolutePath(), options);
+
+        //calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        //real decode bitmap
+        options.inJustDecodeBounds = false;
+
+        Bitmap scaledBitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath(), options);
+
+        //rotation of the image
+        ExifInterface exif = new ExifInterface(imageFile.getAbsolutePath());
+        int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, 0);
+        Matrix matrix = new Matrix();
+        if (orientation == 6) {
+            matrix.postRotate(90);
+        } else if (orientation == 3) {
+            matrix.postRotate(180);
+        } else if (orientation == 8) {
+            matrix.postRotate(270);
+        }
+        scaledBitmap = Bitmap.createBitmap(scaledBitmap, 0, 0, scaledBitmap.getWidth(),
+                scaledBitmap.getHeight(), matrix, true);
+
+        return scaledBitmap;
+    }
+
+    //get inSampleSize
+    private static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+/*
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;*/
+
+            //only 2 *
+            while ((height / inSampleSize) >= reqHeight || (width / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
+    //compress size
+    private static ByteArrayOutputStream compressBitmapSize(Bitmap bitmap, Bitmap.CompressFormat compressFormat,
+                                                            int defaultQuality, long maxSize) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        int quality = defaultQuality;
+        bitmap.compress(compressFormat, quality, baos);
+        while (baos.toByteArray().length / 1024 > maxSize) {
+            if (quality <= 10) { // quality must >0
+                break;
+            } else {
+                baos.reset();
+                quality -= 10;
+                bitmap.compress(compressFormat, quality, baos);
+            }
+        }
+
+        return baos;
+    }
+
+
+}
